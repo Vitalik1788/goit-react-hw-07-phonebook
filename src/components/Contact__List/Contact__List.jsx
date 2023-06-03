8bb908544bfe1afe66def44231c3f8cfa17605dd
Vitalik1788 <tarasenkovitalik2013@ukr.net>
2023-06-03 09:46:57 +0300

    Update Contact__List.jsx

--- src/components/Contact__List/Contact__List.jsx
+++ src/components/Contact__List/Contact__List.jsx
@@ -1,11 +1,11 @@
 import { ContactItem, DeleteBtn } from './Contact__List.styled';
 import { BsFillBookmarkCheckFill } from 'react-icons/bs';
 // import { removeUserContact } from 'redux/contactsReducer';
-import { useDispatch, useSelector } from 'react-redux';
+import {  useSelector } from 'react-redux';
 import { selectContacts } from 'redux/selectors';
 
 const ContactsList = () => {
-  const dispatch = useDispatch();
+  // const dispatch = useDispatch();
   const filteredContacts = useSelector(selectContacts);  
   
   return (
